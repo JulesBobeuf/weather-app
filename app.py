da590b8827f6a1ac03a49bc0b98cafebe3348e4f
Jules Bobeuf <jules_bobeuf@ens.univ-artois.fr>
2022-09-23 15:28:11 +0200

    ajout debut de la bd + bd.db

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
+import sqlite3 as sql
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+def createDatabase():
+    con = sql.connect('bd.db')
+    cur = con.cursor()
+    cur.execute('''CREATE TABLE VILLE ( idVille int primary key, nomVille varchar, nomPays varchar);''')
+    cur.execute('''CREATE TABLE RELEVE ( idReleve int primary key, temperature int, dateDuReleve date, idVille varchar REFERENCES VILLE(idVille));''')
+    con.commit()
+    con.close()
 
 if __name__ == '__main__':
     app.run()
