6c853a168963830daa93b6ba6b736122c8ad92ee
AymericJak <aymerictravail129@gmail.com>
2022-09-23 18:14:51 +0200

    Ajout des fonctions de requete

--- app.py
+++ app.py
@@ -8,13 +8,32 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+
 def requeteToJson(ville):
     r = requests.get(f'https://wttr.in/{ville}?format=j1')
-    return r.json()
+    infosJson = r.json()
+    return infosJson
+
+
+def infosVille(jsonRequest):
+    r = jsonRequest['nearest_area'][0]
+    nomVille = r['areaName'][0]['value']
+    nomPays = r['country'][0]['value']
+    return [nomVille, nomPays]
+
+
+def infosReleve(jsonRequest):
+    r = jsonRequest['current_condition'][0]
+    temperature = r['temp_C']
+    date = r['localObsDateTime'][0:9]
+    heure = r['localObsDateTime'][11:19]
+    return [temperature, date, heure]
+
 
 @app.route('/json')
 def affichage():
-    return requeteToJson('Lens')
+    maRequete = requeteToJson('Lens')
+    return [infosVille(maRequete), infosReleve(maRequete)]
 
 
 if __name__ == '__main__':
